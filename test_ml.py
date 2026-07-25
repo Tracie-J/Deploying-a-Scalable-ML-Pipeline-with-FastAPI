5460d9a208c0022da7d88b3fd97591b4564487ad
Tracie-J <johnsont5595@gmail.com>
2026-07-25 01:43:44 +0000

    Create unit test

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,54 @@
 import pytest
 # TODO: add necessary import
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from ml.model import train_model, compute_model_metrics
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_train_model_returns_random_forest():
     """
-    # add description for the first test
+    Verify that train_model returns a RandomForestClassifier.
     """
     # Your code here
-    pass
+    # create fake training dataset
+    X_train = pd.DataFrame({
+        "feature1": [0, 1, 0, 1],
+        "feature2": [1, 0, 1, 0]
+    })
+    y_train = [0, 1, 0, 1]
+
+    model = train_model(X_train, y_train)
+
+    assert isinstance(model, RandomForestClassifier)
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_compute_model_metrics_returns_floats():
     """
-    # add description for the second test
+    Verify that compute_model_metrics returns numeric metric values
     """
     # Your code here
-    pass
+    y = [0, 1, 1, 0]
+    preds = [0, 1, 0, 0]
+
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+
+    assert isinstance(precision, float)
+    assert isinstance(recall, float)
+    assert isinstance(fbeta, float)
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_compute_model_metrics_range():
     """
-    # add description for the third test
+    Verify that the computed metrics are between 0 and 1.
     """
     # Your code here
-    pass
+    y = [0, 1, 1, 0]
+    preds = [0, 1, 0, 0]
+
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+
+    assert 0 <= precision <= 1
+    assert 0 <= recall <= 1
+    assert 0 <= fbeta <= 1
